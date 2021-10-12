960ed05219c2fd5b583149d42a47553ad1fbd380
Thales Eduardo <thalesdev22@gmail.com>
2021-10-12 10:35:18 -0300

    terminado page Check.

--- src/pages/Check/styles.ts
+++ src/pages/Check/styles.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  padding: 10px;
+`;
+
+export const IdNotepad = styled(FlatList as new () => FlatList)``;
